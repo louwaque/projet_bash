830d1b0169c7cacc6b8ca39c4c3cd77158b96cae
L0L022 <L0L022@openmailbox.org>
2016-11-03 16:16:47 +0100

    on peut prendre les dossiers de n importe ou

--- main.bash
+++ main.bash
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 HASH_FILE="/tmp/bighash"
-MAIN_DIR="$PWD"
+MAIN_FIRST_DIR="$PWD"
 FIRST_DIR="dossier_1"
+MAIN_SECOND_DIR="$PWD"
 SECOND_DIR="dossier_2"
 
-# c pa b1 mm dosier  para
-if [[ -d "$1" && -d "$2" && "$(dirname "$1")" = "$(dirname "$2")" ]]; then
-  MAIN_DIR="$(dirname "$(realpath "$1")")"
+if [[ -d "$1" && -d "$2" ]]; then
+  MAIN_FIRST_DIR="$(dirname "$(realpath "$1")")"
   FIRST_DIR="$(basename "$1")"
+  MAIN_SECOND_DIR="$(dirname "$(realpath "$2")")"
   SECOND_DIR="$(basename "$2")"
 fi
 
 export HASH_FILE
-export MAIN_DIR
+export MAIN_FIRST_DIR
 export FIRST_DIR
+export MAIN_SECOND_DIR
 export SECOND_DIR
 
 if [ -f "$HASH_FILE" ]; then
@@ -39,7 +41,7 @@ function make_hash {
 }
 
 function compare_hash {
-  different_files="$(cat "$HASH_FILE" | sed -e "s|$MAIN_DIR/$FIRST_DIR||g" -e "s|$MAIN_DIR/$SECOND_DIR||g" | sort | uniq -u)"
+  different_files="$(cat "$HASH_FILE" | sed -e "s|$MAIN_FIRST_DIR/$FIRST_DIR||g" -e "s|$MAIN_SECOND_DIR/$SECOND_DIR||g" | sort | uniq -u)"
 
   modified_files="$(echo "$different_files" | cut -d ' ' -f 3 | sort | uniq -d)"
 
@@ -51,12 +53,12 @@ function compare_hash {
   echo "new_files:"
   if [[ ! -z "$new_files" ]]; then
     while read line; do
-      cat "$HASH_FILE" | grep "^[[:alnum:]]\{32\}[[:space:]]\{2\}$MAIN_DIR/\($FIRST_DIR\|$SECOND_DIR\)$line$" | cut -d ' ' -f 3 | sed "s|$MAIN_DIR||g"
+      cat "$HASH_FILE" | grep "^[[:alnum:]]\{32\}[[:space:]]\{2\}\($MAIN_FIRST_DIR/$FIRST_DIR\|$MAIN_SECOND_DIR/$SECOND_DIR\)$line$" | cut -d ' ' -f 3 | sed -e "s|$MAIN_FIRST_DIR||g" -e "s|$MAIN_SECOND_DIR||g"
     done < <(echo "$new_files")
   fi
 }
 
-make_hash "$MAIN_DIR/$FIRST_DIR"
-make_hash "$MAIN_DIR/$SECOND_DIR"
+make_hash "$MAIN_FIRST_DIR/$FIRST_DIR"
+make_hash "$MAIN_SECOND_DIR/$SECOND_DIR"
 
 compare_hash
