4e0617bf295032489f9189a752d3e2ead06d7cf0
L0L022 <L0L022@openmailbox.org>
2016-11-03 16:29:45 +0100

    met plein de commentaires !

--- main.bash
+++ main.bash
@@ -6,6 +6,7 @@ FIRST_DIR="dossier_1"
 MAIN_SECOND_DIR="$PWD"
 SECOND_DIR="dossier_2"
 
+#si l'utilisateur veut comparer des dossiers spécifiques, sinon c'est dossier_1 et dossier_2 qui sont utilisés
 if [[ -d "$1" && -d "$2" ]]; then
   MAIN_FIRST_DIR="$(dirname "$(realpath "$1")")"
   FIRST_DIR="$(basename "$1")"
@@ -23,17 +24,21 @@ if [ -f "$HASH_FILE" ]; then
   rm "$HASH_FILE"
 fi
 
+#hach tous les fichiers contenuent dans le repertoire donné en parametre et irra meme visiter les sous dossiers !
 function make_hash {
   local path
   for path in "$1"/*; do
     if [ -d "$path" ]; then
       if [ "$(ls -A "$path")" ]; then
+        #si c'est un dossier pas vide alors on part hacher les fichiers qui sont à l'interieur
         make_hash "$path"
       else
+        #si c'est un dossier vide alors on le rajoute au hach parce qu'il a besoin d'exister
         echo "" | md5sum | sed "s|-|$path/|g" >> "$HASH_FILE"
       fi
     else
       if [ -f "$path" ]; then
+        #si c'est un fichier alors on le hache
         md5sum "$(realpath "$path")" >> "$HASH_FILE"
       fi
     fi
@@ -41,10 +46,13 @@ function make_hash {
 }
 
 function compare_hash {
+  #contient la liste des fichiers qui peuvent être modifiés ou qui existent que d'un seul coté
   different_files="$(cat "$HASH_FILE" | sed -e "s|$MAIN_FIRST_DIR/$FIRST_DIR||g" -e "s|$MAIN_SECOND_DIR/$SECOND_DIR||g" | sort | uniq -u)"
 
+  #liste des fichiers modifiés
   modified_files="$(echo "$different_files" | cut -d ' ' -f 3 | sort | uniq -d)"
 
+  #liste des fichiers qui existent que dans un seul des deux repertoires
   new_files="$(echo "$different_files" | cut -d ' ' -f 3 | sort | uniq -u)"
 
   echo "modified_files:"
